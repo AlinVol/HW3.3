817644a5c273518f71e67462d3963f9f671cd809
AlinVol <168984806+AlinVol@users.noreply.github.com>
2024-05-19 17:25:57 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6]
+result = [numbers[:3], numbers[3:]]
+print(result)
+
+numbers = [1, 2, 3]
+result = [numbers[:2], numbers[2:]]
+print(result)
+
+numbers = []
+result = [numbers[:3], numbers[3:]]
+print(result)
